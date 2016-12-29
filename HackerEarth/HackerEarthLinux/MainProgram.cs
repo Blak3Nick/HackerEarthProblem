9a2b5b40265ac1807768075a1326cfad0a5edab0
Blak3Nick <blaken2021@gmail.com>
2016-12-29 16:43:32 -0500

    setting up the files on a new computer

---
+++ HackerEarth/HackerEarthLinux/MainProgram.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace StringChallenge
+{
+    public class MainProgram 
+    {
+
+       
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello Blake");
+        }
+    }
+    
+}
