803069e1f3fa68f0b59ed83008aedf6d0f7fda34
Blak3Nick <blaken2021@gmail.com>
2016-12-29 15:27:52 -0500

    working with string challenges

--- HackerEarth/Program.cs
+++ HackerEarth/Program.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Diagnostics.Tracing;
 
 namespace HackerEarth
 {
     public class Program
     {
+       
         public static void Main(string[] args)
         {
+            StringChallenge myString = new StringChallenge();
+            Console.WriteLine(myString.returnString());
         }
+        
     }
 }
---
+++ HackerEarth/StringChallenge.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackerEarth
+{
+
+    public class StringChallenge
+    {
+        public string aString = "abcdE";
+
+        public string[] returnString()
+        {
+            string[] substring = aString.Split();
+            foreach (string letter in substring)
+            {
+                letter.ToUpper();
+            }
+            return substring;
+        } 
+        
+
+
+    }
+}
