40006fe5502203f5d7c84acb13f8011363b99f3e
Blak3Nick <blaken2021@gmail.com>
2016-12-31 16:08:45 -0500

    added product solution

--- HackerEarth/HackerEarthLinux/Product/Product.cs
+++ HackerEarth/HackerEarthLinux/Product/Product.cs
@@ -6,15 +6,22 @@ public class Product
     public static void Main(string[] args)
     {
         var input = Console.ReadLine();
+        int num = Int32.Parse(input);
         string input2 = Console.ReadLine();
-        string s = input2;
-        Console.WriteLine(input);
-        Console.WriteLine(input2);
         char[] params1 = {' '};
-        int[] intArray = new int[input.Length];
-        foreach(int a in intArray)
-        {Console.Write(a);}
-        //string[] = input2.S
+        int[] intArray = new int[num];
+
+        string[] strArray= input2.Split(params1);
+        for (int i =0; i<strArray.Length;i++)
+        {
+            intArray[i] = Int32.Parse(strArray[i]);
+        }
+        int product = 1;
+        foreach(int b in intArray)
+        {
+            product = product * b;
+        }
+        Console.WriteLine(product);
         
     }
 }
