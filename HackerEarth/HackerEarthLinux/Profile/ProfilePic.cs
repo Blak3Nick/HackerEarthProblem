1e9c5ee183392aa922be0c7c609d0bed8d76a356
Blak3Nick <blaken2021@gmail.com>
2017-01-02 16:29:33 -0500

    added new methods

--- HackerEarth/HackerEarthLinux/Profile/ProfilePic.cs
+++ HackerEarth/HackerEarthLinux/Profile/ProfilePic.cs
@@ -3,10 +3,30 @@ namespace HackerEarth
 {
 	public class ProfilePic
 	{
-		public void Main(string[] args)
+		public static void Main(string[] args)
 		{
-		string L = Console.ReadLine();
-			Console.WriteLine (L);
+			string[] myArray = new string[5];
+			string L = Console.ReadLine ();
+			int Lint = Int32.Parse (L);
+			string W = Console.ReadLine ();
+			int Wint = Int32.Parse (W);
+			int counter = Wint;
+			while (counter != 0) {
+				
+
+				Array.Resize (ref myArray, Wint);
+
+				for (int i = 0; i < Wint; i++) {
+					myArray [i] = Console.ReadLine ();
+					counter--;
+				}
+
+			}
+				foreach (string a in myArray) {
+					Console.WriteLine (a);
+				}
+				
+
 		}
 	}
 }
