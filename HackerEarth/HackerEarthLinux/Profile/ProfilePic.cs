f751c9351f07350a83b9521b3dcf12a76ae561e7
Blak3Nick <blaken2021@gmail.com>
2017-01-02 15:20:11 -0500

    added new solution file for profile pic challenge

---
+++ HackerEarth/HackerEarthLinux/Profile/ProfilePic.cs
@@ -0,0 +1,12 @@
+using System;
+namespace HackerEarth
+{
+	public class ProfilePic
+	{
+		public void Main(string[] args)
+		{
+		string L = Console.ReadLine();
+			Console.WriteLine (L);
+		}
+	}
+}
