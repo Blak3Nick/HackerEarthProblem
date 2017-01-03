0f36ed94b82ea0d8c7fbe930af0fce4e15aff08a
Blak3Nick <blaken2021@gmail.com>
2017-01-03 18:39:26 -0500

    new files

--- HackerEarth/HackerEarthLinux/Profile/ProfilePic.cs
+++ HackerEarth/HackerEarthLinux/Profile/ProfilePic.cs
@@ -1,10 +1,16 @@
 using System;
+using System.Collections.Generic;
 namespace HackerEarth
 {
 	public class ProfilePic
 	{
 		public static void Main(string[] args)
 		{
+			/*[1] If any of the width or height is less than L, user is prompted to upload another one. Print "UPLOAD ANOTHER" in this case.
+			[2] If width and height, both are large enough and
+			(a) if the photo is already square then it is accepted. Print "ACCEPTED" in this case.
+			(b) else user is prompted to crop it. Print "CROP IT" in this case. */
+			char[] paraams = {' ' };
 			string[] myArray = new string[5];
 			string L = Console.ReadLine ();
 			int Lint = Int32.Parse (L);
@@ -22,7 +28,12 @@ namespace HackerEarth
 				}
 
 			}
-				foreach (string a in myArray) {
+			string[] doubled = new string[Wint * 2];
+			List<string> myList = new List<string>();
+			for (int i = 0; i < Wint; i++) {
+				myList.Add( myArray [i].Split (paraams));
+			}
+			foreach (string a in myList) {
 					Console.WriteLine (a);
 				}
 				
