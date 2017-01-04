fa5b1003646fbcb1f347b0a8baa674adf1f82fba
Blak3Nick <blaken2021@gmail.com>
2017-01-03 19:55:01 -0500

    added new file for factorial solution

--- HackerEarth/HackerEarthLinux/Factorial/Factorial.cs
+++ HackerEarth/HackerEarthLinux/Factorial/Factorial.cs
@@ -1,1 +1,20 @@
 using System;
+public class Factorial
+{
+	public static void Main(string[] args)
+	{
+		var input = Console.ReadLine ();
+		int myInt = Int32.Parse (input);
+		int toReturn = 1;
+		int[] myArray = new int[myInt];
+		for (int i = myInt; i > 1; i--) {
+			myArray [i - 1] = i;
+
+		}
+		myArray[0] = 1;
+		foreach (int a in myArray) {
+			toReturn = toReturn * a;
+		}
+		Console.WriteLine (toReturn);
+	}
+}
